2037e3d55a4e4a9c0a1d82868bb7b16026fa62fc
smunoz <sgmunoz89@gmail.com>
2019-11-23 14:19:21 -0300

    conociendo los ciclos de vida del componente

--- src/components/ListaProductos.js
+++ src/components/ListaProductos.js
@@ -3,14 +3,46 @@ import Producto from './Producto'
 
 class ListaProductos extends Component {
     state = {
-        productos : [
-            {id : 1, nombre : 'camisa reactjs', precio : 30 },
-            {id : 2, nombre : 'camisa vuejs', precio : 30 },
-            {id : 3, nombre : 'camisa angularjs', precio : 30 },
-            {id : 4, nombre : 'camisa nodejs', precio : 30 },
-        ]
+        productos : []
         
     }
+    /* Estos componentes 
+       se cargan 
+       1 .- componentWillMount()
+       2 .- render()
+       3.-  componentDidMount()
+
+
+       si hay cambio 
+       componentWillUpdate()
+       render()
+       componentDidMount()
+       
+    */
+
+    componentDidMount(){
+        /* cuando esta listo el documento */
+        this.setState({
+            productos : [
+                            {id : 1, nombre : 'camisa reactjs', precio : 30 },
+                            {id : 2, nombre : 'camisa vuejs', precio : 30 },
+                            {id : 3, nombre : 'camisa angularjs', precio : 30 },
+                            {id : 4, nombre : 'camisa nodejs', precio : 30 }
+                        ]
+        })
+    }
+    componentWillMount(){
+        /* antes que este listo */
+
+    }
+    componentWillUpdate(){
+        /* cuando se actualiza */
+    }
+
+    componentWillUnmount(){
+        /* cuando el componente es removido */
+    }
+
     render() {
 
         const {productos} = this.state
