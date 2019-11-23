8168895f1bc52f86f94edc7c7c695a38fc71b6d1
smunoz <sgmunoz89@gmail.com>
2019-11-23 10:49:49 -0300

    Componentes funcional

--- src/components/PrimerComponente.js
+++ src/components/PrimerComponente.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
 
 
-class PrimerComponente extends Component {
-    render() {
-        return (
-            <h1>Hola mundo</h1>
-        );
-    }
+    // class PrimerComponente extends Component {
+    //     render() {
+    //         return (
+    //             <h1>Hola mundo</h1>
+    //         );
+    //     }
+    // }
+
+const PrimerComponente = () => {
+    return (
+        <h1>Hola mundo</h1>
+    );
 }
 
 export default PrimerComponente;
